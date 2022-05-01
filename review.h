bbf9dc260823edbd7a22d27075ab402980edaf89
21900279 <100542967+21900279@users.noreply.github.com>
2022-05-01 20:11:52 +0900

    add review.h

---
+++ review.h
@@ -0,0 +1,18 @@
+#include <stdio.h>
+typedef struct{  
+  char nikename[20];    // 작성자 닉네임
+  char content[200];    // 리뷰 내용
+  bool recommend;       // 추천 여부
+  int rating;           // 별점 (1 ~ 5)
+}Review;
+
+void write(Review* review[], int count);            // 리뷰를 작성한다. 입력값은 리뷰 구조체 배열과 갯수이다.
+void read_all(Review* review[], int count);         // 리뷰 목록을 전체 조회. 입력값은 리뷰 구조체 배열과 갯수이다.
+void read_recommend(Review* review[], int count);   // 리뷰 목록중 추천 여부가 true인 목록만 조회. 입력값은 리뷰 구조체 배열과 갯수이다.
+void read_conditional(Review* review[], int count); // 리뷰 목록중 별점을 기준으로 조건부로 조회한다. 입력값은 리뷰 구조체 배열과 갯수이다.
+void read(Review* review[], int count);             // 리뷰 목록을 조회한다. 입력값은 리뷰 구조체 배열과 갯수이다.
+void search(Review* review[], int count);           // 리뷰의 작성자를 검색한다. 입력값은 리뷰 구조체 배열과 갯수이다.
+void edit(Review* review[], int count);             // 리뷰를 수정한다. 입력값은 리뷰 구조체 배열과 갯수이다.
+void del(Review* review[], int count);              // 리뷰를 삭제한다. 입력값은 리뷰 구조체 배열과 갯수이다.
+void save(Review* review[], int count);             // 리뷰 목록을 파일에 저장한다. 입력값은 리뷰 구조체 배열과 갯수이다.
+int load(Review* review[]);                         // 리뷰 목록을 파일에서 불러온다. 입력값은 리뷰 구조체 배열이다.
